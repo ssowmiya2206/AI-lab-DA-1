ba67694eeea28b84785264008444e4396f445ef2
Sowmiya <sowmiyasow06@gmail.com>
2026-01-24 17:14:33 +0530

    Update BFS.py

--- BFS.py
+++ BFS.py
@@ -1,31 +1,42 @@
 from collections import deque
 
-def bfs(graph, start):
+# Graph represented as an adjacency list
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+
+def bfs(graph, start_node):
     visited = set()
-    queue = deque([start])
-
-    print("BFS Traversal:", end=" ")
-
+    queue = deque([start_node])
+    visited.add(start_node)
+    
+    order = []
     while queue:
-        node = queue.popleft()
-        if node not in visited:
-            print(node, end=" ")
-            visited.add(node)
-            for neighbor in graph[node]:
+        vertex = queue.popleft()
+        order.append(vertex)
+        
+        for neighbor in graph[vertex]:
+            if neighbor not in visited:
+                visited.add(neighbor)
                 queue.append(neighbor)
+    return order
 
+def dfs(graph, start_node, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start_node)
+    
+    # Process node
+    for next_node in set(graph[start_node]) - visited:
+        dfs(graph, next_node, visited)
+    return visited
 
-graph = {}
-n = int(input("Enter number of nodes: "))
-
-for _ in range(n):
-    node = input("Enter node: ")
-    graph[node] = []
-
-e = int(input("Enter number of edges: "))
-for _ in range(e):
-    u, v = input("Enter edge (u v): ").split()
-    graph[u].append(v)
-
-start = input("Enter start node: ")
-bfs(graph, start)
+if __name__ == "__main__":
+    print("BFS Order:", bfs(graph, 'A'))
+    # DFS order can vary based on set subtraction order
+    print("DFS Visited:", list(dfs(graph, 'A')))
